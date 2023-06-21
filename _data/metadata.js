c80ad7ef1fc74b95a932a0b9136e224d71425861
Jin Yang <jinkunyang200627@googlemail.com>
2023-06-21 15:10:59 +0100

    completed-part-1

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Jin's Tech Blog",
+	url: "https://jinstechblog.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about cool new tech.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Jin Yang",
+		email: "jinkunyang2006@hotmail.co.uk",
+		url: "https://jinstechblog.com/aboutme/"
 	}
 }
